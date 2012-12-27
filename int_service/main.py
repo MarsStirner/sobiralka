9096322de0429af9b93a9ed88c2bd735289fdfc2
SAntipov <santipov@korusconsulting.ru>
2012-12-27 15:50:45 +0400

    Added python path

--- int_service/main.py
+++ int_service/main.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, 'int_service'))
+sys.path.insert(0, root_path)
+
 from lib.soap_server import Server
 
 def main():
