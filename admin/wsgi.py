c2389ac913636f703d4401b9395453b5214f0d8f
SAntipov <santipov@korusconsulting.ru>
2013-01-23 19:30:36 +0400

    Deploying IS admin

--- admin/wsgi.py
+++ admin/wsgi.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys, os
+sys.path.insert(0, os.path.abspath('..'))
+
 from admin import app as application, admin
 from admin.database import Session
 import views
 
-sys.path.insert(0, os.path.dirname(__file__))
 
 admin.init_app(application)
 admin.add_view(views.LPUAdmin(Session, name=u'Список ЛПУ'))
