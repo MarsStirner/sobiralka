437b1b0cc8cf608f6b85fb2ad147e268f77abacd
SAntipov <santipov@korusconsulting.ru>
2013-01-23 19:59:56 +0400

    Deploying IS admin

--- admin/wsgi.py
+++ admin/wsgi.py
@@ -2,9 +2,8 @@
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 
-from admin import app as application, admin
+from admin import app as application, admin, views
 from admin.database import Session
-import views
 
 
 admin.init_app(application)
