20e0ae5b7915ae4b1a3b7e489423ac6ddbeab274
SAntipov <santipov@korusconsulting.ru>
2013-01-24 19:04:22 +0400

    debugging deploying script

--- fabfile.py
+++ fabfile.py
@@ -25,14 +25,17 @@ def configure_db():
     queries.append( "CREATE DATABASE %s;" % DB_NAME)
 #    local('echo "CREATE DATABASE %s;" | mysql -h %s -u %s -p' % (DB_NAME, DB_HOST, user))
     #Создаём пользователя для работы с БД
+    db_user_host = DB_HOST
+    if db_user_host not in ('localhost', '127.0.0.1'):
+        db_user_host = '%'
     if DB_USER != 'root':
-        queries.append("CREATE USER '%s'@'%s' IDENTIFIED BY '%s';" % (DB_USER, DB_HOST, DB_PASSWORD))
+        queries.append("CREATE USER '%s'@'%s' IDENTIFIED BY '%s';" % (DB_USER, db_user_host, DB_PASSWORD))
 #        local(
 #            '''echo "CREATE USER '%s'@'%s' IDENTIFIED BY '%s';" | mysql -h %s -u %s -p''' %
 #            (DB_USER, DB_HOST, DB_PASSWORD, DB_HOST, user)
 #        )
     #Выдаём пользователю привелегии на работу с БД
-    queries.append("GRANT ALL PRIVILEGES ON %s.* TO '%s'@'%s';" % (DB_NAME, DB_USER, DB_HOST))
+    queries.append("GRANT ALL PRIVILEGES ON %s.* TO '%s'@'%s';" % (DB_NAME, DB_USER, db_user_host))
 #    local(
 #        '''echo "GRANT ALL PRIVILEGES ON %s.* TO '%s'@'%s';" | mysql -h %s -u %s -p''' %
 #        (DB_NAME, DB_USER, DB_HOST, DB_HOST, user)
