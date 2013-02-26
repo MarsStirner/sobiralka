28e20160cbfa9322fbddbe848235fe5c324e27ca
Serge Antipov <antipov.serge@gmail.com>
2013-02-26 17:33:07 +0400

    Update admin/wsgi.py
    for windows thats kind of path is correct

--- admin/wsgi.py
+++ admin/wsgi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys, os
-sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from app import app as application
