8c3240e8e74340b08a43fadc9fa89cbf8e78b6a1
Serge Antipov <antipov.serge@gmail.com>
2013-05-08 17:27:32 +0400

    Update service_clients.py

--- int_service/lib/service_clients.py
+++ int_service/lib/service_clients.py
@@ -8,7 +8,7 @@ import logging
 from urlparse import urlparse
 from abc import ABCMeta, abstractmethod, abstractproperty
 from suds.client import Client
-from suds.plugin import MessagePlugin
+#from suds.plugin import MessagePlugin
 from suds.cache import DocumentCache
 from suds import WebFault
 import is_exceptions
