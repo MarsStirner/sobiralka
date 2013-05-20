c21a1b3382a9c1cf8d7f2a126891eecbc7a2596e
SAntipov <santipov@korusconsulting.ru>
2013-05-20 17:50:28 +0400

    Some optimization

--- int_service/lib/dataworker.py
+++ int_service/lib/dataworker.py
@@ -1368,7 +1368,6 @@ class UpdateWorker(object):
     def update_data(self):
         """Основной метод, который производит вызов внутренних методов обновления данных в БД ИС"""
         # self.session.begin()
-        self.__init_database()
         # Update data in tables
         lpu_dw = LPUWorker()
         lpu_list = lpu_dw.get_list()
