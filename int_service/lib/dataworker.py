1f0910dc3b7777f080ef301bb145583a0324d5a0
SAntipov <santipov@korusconsulting.ru>
2013-10-25 11:42:20 +0400

    Added #TODO

--- int_service/lib/dataworker.py
+++ int_service/lib/dataworker.py
@@ -1646,6 +1646,7 @@ class EPGUWorker(object):
         result = []
         epgu_result = self.proxy_client.GetLocations(hospital)
         locations = getattr(epgu_result, 'locations', None)
+        #TODO: проверить возвращаемое значение, а то возникает ошибка в отсутствии атрибуту location у locations
         if locations:
             result.extend(self.__get_location_data(locations))
             num_pages = int(locations.paginate.num_pages)
