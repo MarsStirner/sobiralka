e7e571834095443fb2dd7de87f365439c8b023af
SAntipov <santipov@korusconsulting.ru>
2013-12-19 18:24:31 +0400

    Version update

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'2.3.0'
-last_change_date = datetime(year=2013, month=12, day=4, hour=19,)
+version = u'2.4.0'
+last_change_date = datetime(year=2013, month=12, day=6, hour=19,)
